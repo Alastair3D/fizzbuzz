09bff5da7ccbeba392afdcebc5bd28e25ce55967
Possed <r0xana.pirvu@yahoo.com>
2018-04-14 16:49:05 +0100

    completed fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz(number)
-  'fizz'
+def fizzbuzz(n)
+  if n % 15 == 0
+    return 'fizzbuzz'
+  elsif n % 3 == 0
+    return 'fizz'
+  elsif n % 5 == 0
+    return 'buzz'
+  else
+    return n
+  end
 end
